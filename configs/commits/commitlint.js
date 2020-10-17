1226bc1820b70aa17586c201b2b97f6ce75e700b
CitRUS <citrus4009@gmail.com>
2020-10-18 01:33:32 +0300

    fix(commitlint): Fix scope case

--- configs/commits/commitlint.js
+++ configs/commits/commitlint.js
@@ -5,7 +5,7 @@ module.exports = {
         "body-max-length": [2, "always", 0],
         "footer-leading-blank": [2, "always"],
         "header-max-length": [2, "always", 72],
-        "scope-case": [2, "always", "lower-case"],
+        "scope-case": [2, "always", "kebab-case"],
         "subject-case": [2, "always", "sentence-case"],
         "subject-empty": [2, "never"],
         "subject-full-stop": [2, "never", "."],
