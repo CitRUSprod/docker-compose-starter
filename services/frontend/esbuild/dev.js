5b50f73b0c819d58022b830db652d41adb206bdd
CitRUS <citrus4009@gmail.com>
2021-03-23 18:44:11 +0300

    fix(frontend): Add JSON.stringify for env values

--- services/frontend/esbuild/dev.js
+++ services/frontend/esbuild/dev.js
@@ -2,16 +2,16 @@ const path = require("path")
 const dotenv = require("dotenv")
 const { build } = require("./base")
 
-const { parsed: parsedEnvs } = dotenv.config({
+const { parsed: parsedEnv } = dotenv.config({
     path: path.join(__dirname, "../../../.env")
 })
 
-function createProcessEnvs(envs) {
-    if (envs) {
+function createProcessEnv(env) {
+    if (env) {
         const result = {}
 
-        for (const [key, value] of Object.entries(envs)) {
-            result[`process.env.${key}`] = value
+        for (const [key, value] of Object.entries(env)) {
+            result[`process.env.${key}`] = JSON.stringify(value)
         }
 
         return result
@@ -23,7 +23,7 @@ function createProcessEnvs(envs) {
 const devConfig = {
     sourcemap: "inline",
     define: {
-        ...createProcessEnvs(parsedEnvs),
+        ...createProcessEnv(parsedEnv),
         "process.env.NODE_ENV": JSON.stringify("development")
     },
     inject: [path.join(__dirname, "sourcemaps.js")]
