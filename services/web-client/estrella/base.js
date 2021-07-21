a7948780d9de4441b1f44bbdfea2ff1c6596cdb5
CitRUS <citrus4009@gmail.com>
2021-07-21 15:36:33 +0300

    build(web-client): Add cleanup of dist on build

--- services/web-client/estrella/base.js
+++ services/web-client/estrella/base.js
@@ -1,12 +1,18 @@
 const path = require("path")
 const { nodeExternalsPlugin } = require("esbuild-node-externals")
+const { default: cleanPlugin } = require("esbuild-plugin-clean")
 
 const baseConfig = {
     entry: path.join(__dirname, "../src/app.ts"),
     outfile: path.join(__dirname, "../dist/app.js"),
     bundle: true,
     platform: "node",
-    plugins: [nodeExternalsPlugin()]
+    plugins: [
+        nodeExternalsPlugin(),
+        cleanPlugin({
+            patterns: ["dist/*"]
+        })
+    ]
 }
 
 module.exports = baseConfig
