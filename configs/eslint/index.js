568b9bd1dbacac812f5c1daba97726facc220f6e
CitRUS <citrus4009@gmail.com>
2021-08-22 23:17:15 +0300

    chore: Switch prettier to eslint-plugin-prettier

--- configs/eslint/index.js
+++ configs/eslint/index.js
@@ -1,11 +1,33 @@
-module.exports = {
+/** @type {import("eslint").Linter.Config} */
+const config = {
     extends: ["@citrus-linting", "@citrus-linting/eslint-config/typescript", "prettier"],
+    plugins: ["prettier"],
+    rules: {
+        "prettier/prettier": [2]
+    },
     overrides: [
         {
-            files: "*.ts",
+            files: ["*.json"],
+            parser: "any-eslint-parser"
+        },
+        {
+            files: ["*.y?(a)ml"],
+            parser: "any-eslint-parser"
+        },
+        {
+            files: ["*.md"],
+            parser: "any-eslint-parser",
+            rules: {
+                "prettier/prettier": [2, { parser: "markdown" }]
+            }
+        },
+        {
+            files: ["*.ts"],
             parserOptions: {
                 project: "./services/*/tsconfig.json"
             }
         }
     ]
 }
+
+module.exports = config
