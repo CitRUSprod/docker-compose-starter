b0d7e2b2943d273128996e4f234a5554f53e0985
CitRUS <citrus4009@gmail.com>
2021-09-07 18:08:51 +0300

    feat: Add example of using process.env

--- services/web/src/app.ts
+++ services/web/src/app.ts
@@ -2,12 +2,16 @@ import fastify from "fastify"
 
 const port = 6601
 
+const websitePort = process.env.WEBSITE_PORT!
+
 const app = fastify()
 
 app.get("/", (request, reply) => {
-    reply
-        .type("text/html; charset=utf-8")
-        .send(`<h1>Docker Compose Template</h1><h2>docker-compose-template</h2>`)
+    reply.type("text/html; charset=utf-8").send(`
+        <h1>Docker Compose Template</h1>
+        <h2>docker-compose-template</h2>
+        <h3>WEBSITE_PORT=${websitePort}</h3>
+    `)
 })
 
 app.listen(port, "0.0.0.0", (err: Error | null) => {
